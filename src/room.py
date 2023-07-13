68c252bc0a6983829e76ecd5da16e81ed38528ac
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-13 15:34:01 +0100

    Add room class and test attributes

---
+++ src/room.py
@@ -0,0 +1,7 @@
+class Room:
+    
+    def __init__(self, name, capacity, entry_fee, playlist):
+        self.name = name
+        self.capacity = capacity
+        self.entry_fee = entry_fee
+        self.playlist = playlist
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,7 +1,21 @@
-
-
+from src.room import Room
+from src.song import Song
 import unittest
 
 
 class TestRoom(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.room = Room("Houston Room", 50, 20, [Song("How Will I Know", "Whitney Houston"), Song("Million Dollar Bill", "Whitney Houston"), Song("I Wanna Dance With Somebody", "Whitney Houston")])
+
+    def test_room_has_name(self):
+        self.assertEqual("Houston Room", self.room.name)
+
+    def test_room_has_capacity(self):
+        self.assertEqual(50, self.room.capacity)
+
+    def  test_room_has_entry_fee(self):
+        self.assertEqual(20, self.room.entry_fee)
+
+    def test_room_has_songs(self):
+        self.assertEqual(list, type(self.room.playlist))
