20eebe2f5da9f8a32bdee0c399a2b71d33416b10
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-13 15:19:57 +0100

    Add guest class and test attributes

---
+++ src/guest.py
@@ -0,0 +1,7 @@
+class Guest: 
+    def __init__(self, name, fav_song, money):
+        self.name = name
+        self.fav_song = fav_song
+        self.money = money
+
+    
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,5 +1,18 @@
 import unittest
+from src.guest import Guest
+from src.song import Song
 
 
 class TestGuest(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.guest = Guest("Sam", Song("Happy Birthday", "Stevie Wonder"), 200)
+
+    def test_guest_has_name(self):
+        self.assertEqual("Sam", self.guest.name)
+
+    def test_guest_has_name(self):
+        self.assertEqual("Happy Birthday", self.guest.fav_song.name)
+
+    def test_guest_has_money(self):
+        self.assertEqual(200, self.guest.money)
