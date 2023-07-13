10630089c70fd6b8ab8ed5617a3574b62d8ffc8a
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-13 15:43:58 +0100

    Add check_in_guest function which adds a guest to a room

--- src/room.py
+++ src/room.py
@@ -5,3 +5,7 @@ class Room:
         self.capacity = capacity
         self.entry_fee = entry_fee
         self.playlist = playlist
+        self.guests_in_room = []
+
+    def check_in_guest(self, guest):
+        self.guests_in_room.append(guest)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,11 +1,13 @@
 from src.room import Room
 from src.song import Song
+from src.guest import Guest
 import unittest
 
 
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
+        self.guest = Guest("Gary", Song("Let The Music Play", "Barry White"), 80)
         self.room = Room("Houston Room", 50, 20, [Song("How Will I Know", "Whitney Houston"), Song("Million Dollar Bill", "Whitney Houston"), Song("I Wanna Dance With Somebody", "Whitney Houston")])
 
     def test_room_has_name(self):
@@ -19,3 +21,7 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_songs(self):
         self.assertEqual(list, type(self.room.playlist))
+
+    def test_guest_checked_in(self):
+        self.room.check_in_guest(self.guest)
+        self.assertEqual("Gary", self.room.guests_in_room[0].name)
